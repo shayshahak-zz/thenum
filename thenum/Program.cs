d715aadf25aaa21fbb62ca064c6a4b81a8da5137
Shay Shahak <shays@microsoft.com>
2017-03-02 15:35:17 +0200

    adding comments

--- thenum/Program.cs
+++ thenum/Program.cs
@@ -12,6 +12,8 @@ namespace thenum
         {
             Console.WriteLine("enter a number");
             int num = int.Parse(Console.ReadLine());
+
+            //taking care for the upper part of the pyramid
             for (int i = 0; i <= num; i++)
             {
                 for (int j = 0; j < (num - i); j++)
@@ -25,7 +27,7 @@ namespace thenum
                 Console.WriteLine();
 
             }
-
+            //taking care for the bottom part of the pyramid
             for (int i = num-1; i > 0; i--)
             {
                 for (int j = 0; j < (num-i); j++)
